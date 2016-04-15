b2ceefb761f8b15959b7fad25681bb7cd3728918
Daniel Fiser <danfis@danfis.cz>
2016-04-15 14:56:24 +0200

    mutex: Improvements of h2.

--- src/translate/mutex/h2.py
+++ src/translate/mutex/h2.py
@@ -12,7 +12,7 @@ def gen_meta_atoms(atoms):
     ma += gen_pair_meta_atoms(atoms)
     return ma
 
-class Action(object):
+class P2Action(object):
     def __init__(self, name, pre, add_eff, add_fact = None):
         self.name = name
         if add_fact is None:
@@ -37,13 +37,13 @@ def create_p2_actions(actions, atoms):
         exp = atoms - (set(add_eff) | set(del_eff))
         # TODO: Conditional effects
 
-        p2 += [Action(a.name, pre, add_eff)]
+        p2 += [P2Action(a.name, pre, add_eff)]
         for fact in exp:
-            p2 += [Action(a.name, pre, add_eff, fact)]
+            p2 += [P2Action(a.name, pre, add_eff, fact)]
 
     return p2
 
-def h2(task, atoms, actions):
+def h2p2(task, atoms, actions):
     atoms = common.filter_atoms(atoms)
     actions = create_p2_actions(actions, atoms)
 
@@ -63,6 +63,62 @@ def h2(task, atoms, actions):
     h2_mutex = set(gen_pair_meta_atoms(atoms)) - reached
     unreachable = atoms - reached
     return h2_mutex, unreachable
+
+class H2Action(object):
+    def __init__(self, a):
+        self.name = a.name
+
+        pre = a.precondition
+        add_eff = [x[1] for x in a.add_effects if len(x[0]) == 0]
+        del_eff = [x[1] for x in a.del_effects if len(x[0]) == 0]
+        self.pre1 = set(pre)
+        self.add_eff1 = set(add_eff)
+        self.add_del = set(add_eff) | set(del_eff)
+
+        self.pre2 = set(gen_pair_meta_atoms(self.pre1))
+        p = self.pre1 - self.add_del
+        self.add_eff2 = set(gen_pair_meta_atoms(self.add_eff1 | p))
+
+        self.pre = self.pre1 | self.pre2
+        self.add_eff = self.add_eff1 | self.add_eff2
+
+    def pairs(self, fact, facts):
+        for f in facts:
+            yield tuple(sorted([fact, f]))
+
+    def eff(self, reached):
+        h2 = set(self.add_eff)
+
+        atoms = set([x for x in reached if type(x) is not tuple])
+        atoms -= self.add_del
+        for atom in atoms:
+            pre = set(self.pairs(atom, self.pre1))
+            if pre.issubset(reached):
+                for atom2 in self.add_eff1:
+                    t = tuple(sorted([atom, atom2]))
+                    h2.add(t)
+        return h2
+
+
+def h2(task, atoms, actions):
+    atoms = common.filter_atoms(atoms)
+    actions = [H2Action(a) for a in actions]
+
+    lastsize = 0
+    init = set(task.init) & atoms
+    reached = set(gen_meta_atoms(init))
+    while len(reached) != lastsize:
+        lastsize = len(reached)
+
+        for a in actions:
+            if a.pre.issubset(reached):
+                reached |= a.eff(reached)
+
+    h2 = set(gen_meta_atoms(atoms)) - reached
+    h1 = set([x for x in h2 if type(x) is not tuple])
+    h2 -= h1
+    h2 |= set([tuple([x]) for x in h1])
+    return list(h2)
 
 def h2_max(task, atoms, actions):
     h2_mutex, unreachable = h2(task, atoms, actions)
