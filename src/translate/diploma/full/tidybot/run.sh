e26029f002db5e2637b0faefa4748d7f17bb5286
Daniel Fiser <danfis@danfis.cz>
2016-05-23 13:48:59 +0200

    diploma: Results from exhaustive search.

---
+++ src/translate/diploma/full/tidybot/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#/storage/praha1/home/danfis/dev/fd-full/full.sh /storage/praha1/home/danfis/dev/pddl-data/test-seq/depot/{domain,pfile1}.pddl
+
+#python2 /storage/praha1/home/danfis/dev/fd-full/translate.py /storage/praha1/home/danfis/dev/pddl-data/test-seq/depot/{domain,pfile1}.pddl
+python2 /storage/praha1/home/danfis/dev/fd-full/translate.py /storage/praha1/home/danfis/dev/pddl-data/ipc-2014/seq-opt/tidybot/{domain,p14}.pddl
+mv m.out /storage/praha1/home/danfis/metabench/full/tidybot/
